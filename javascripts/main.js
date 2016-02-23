5346f579c5f6475e3ec7ec8c118413419706ddd5
china007 <396141419@qq.com>
2016-02-23 11:11:55 +0900

    safari设定viewport＝320

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,9 +15,11 @@ if (/Android (\d+\.\d+)/.test(ua)) {
 	document.write('<meta name="viewport" content="width=320, user-scalable=no, target-densitydpi=device-dpi">');
 }
  */
+bowerInfo();
 var page=0;
 var MaxEmoji=90;
 $(function() {
+
 	//取得历史消息
 	getHistory();
 	saveIp();
@@ -29,6 +31,29 @@ $(function() {
 	initEmoji();
 });
 
+/**
+ *
+ */
+ function bowerInfo(){
+		var Sys = {};
+        var ua = navigator.userAgent.toLowerCase();
+        var s;
+        (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] :
+        (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+        (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+        (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+        (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+        
+        /*if (Sys.ie) document.write('IE: ' + Sys.ie);
+        if (Sys.firefox) document.write('Firefox: ' + Sys.firefox);
+        if (Sys.chrome) document.write('Chrome: ' + Sys.chrome);
+        if (Sys.opera) document.write('Opera: ' + Sys.opera);
+        if (Sys.safari) document.write('Safari: ' + Sys.safari);*/
+		if (Sys.safari){
+			document.write('<meta name="viewport" content="width=320">');
+		}
+ }
 /**
  * 显示好友tab
  */
