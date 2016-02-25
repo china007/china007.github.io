eefe207f4b59a80beae6c6b5d108dd8ff0b78021
china007 <396141419@qq.com>
2016-02-25 17:23:59 +0900

    消息提示分类提醒

--- javascripts/main.js
+++ javascripts/main.js
@@ -188,6 +188,12 @@ BmobSocketIo.onUpdateTable = function (tablename, data) {
 			getMsg(data.sendFrom, data.sendTo, data.createdAt, data.content);
 			scollToEnd();
 			if (Sys.chrome) {
+				if(data.content.indexOf("<img")==0){
+					data.content="发送过来一个表情，请打开浏览器查看";
+				}				
+				if(data.content.indexOf("<a")==0){
+					data.content="发送过来一个文件，请打开浏览器查看";
+				}
 				notify(data.content,userList[data.sendFrom].img,userList[data.sendFrom].name + data.createdAt.substring(11));
 			}
 		}
@@ -403,6 +409,10 @@ function spawnNotification(theBody,theIcon,theTitle) {
 		  icon: theIcon
 	  }
 	  var n = new Notification(theTitle,options);
+	  n.onclick = function() {
+                alert("You clicked me!");
+                //window.location = "/";
+      };
 	}
 
 	/**
